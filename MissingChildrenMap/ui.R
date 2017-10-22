8b23c5b219b1aa08ff71e92f654c9da56ad6a300
Connor Jason Segneri <cjsegneri@cox.net>
2017-10-21 22:46:07 -0700

    add shinydashboard ui

--- MissingChildrenMap/ui.R
+++ MissingChildrenMap/ui.R
@@ -1,31 +1,44 @@
 
 library(shiny)
+library(shinydashboard)
 library(leaflet)
 
-                    
-fluidPage(
-  
-  checkboxGroupInput("mapOption",
-                     "Choose which data to add",
-                     choices = c("Missing Children Data" = "miss",
-                                 "Incidents Data" = "inci"),
-                     selected = c("miss","inci")),
-  
-  leafletOutput("mymap"),
-  
-  selectInput("state", "Sort by state", choices = sort(c(missing$`Missing State`)),
-              multiple = TRUE),
-  
-  selectInput("city", "Sort by city", choices = sort(c(missing$`Missing City`,
-                                                       attempts$`Incident City`)),
-              multiple = TRUE),
-  
-  selectInput("zip", "Sort by zip", choices = sort(c(missing$ZIP,
-                                                       attempts$ZIP)),
-              multiple = TRUE),
-  
-  selectInput("caseID", "Sort by case id", choices = sort(c(missing$`Case Number`,
-                                                     attempts$`Case Number`)),
-              multiple = TRUE)
-  
+
+shinyUI(
+  dashboardPage(
+    dashboardHeader(title = "Header 1"),
+    dashboardSidebar(collapsed = FALSE,
+                     sidebarMenu(
+                       menuItem("Drop Down 1", icon = icon("chevron-right"), startExpanded = TRUE,
+                                checkboxGroupInput("mapOption",
+                                                   "Choose which data to add",
+                                                   choices = c("Missing Children Data" = "miss",
+                                                               "Incidents Data" = "inci"),
+                                                   selected = c("miss","inci"))  
+                       )
+                     ),
+                     sidebarMenu(
+                       menuItem("Drop Down 2", icon = icon("chevron-right"), startExpanded = TRUE,
+                                selectInput("state", "Sort by state", choices = sort(c(missing$`Missing State`)),
+                                            multiple = TRUE),
+                                
+                                selectInput("city", "Sort by city", choices = sort(c(missing$`Missing City`,
+                                                                                     attempts$`Incident City`)),
+                                            multiple = TRUE),
+                                
+                                selectInput("zip", "Sort by zip", choices = sort(c(missing$ZIP,
+                                                                                   attempts$ZIP)),
+                                            multiple = TRUE),
+                                
+                                selectInput("caseID", "Sort by case id", choices = sort(c(missing$`Case Number`,
+                                                                                          attempts$`Case Number`)),
+                                            multiple = TRUE)
+                       )
+                     )
+    ),
+    dashboardBody(
+      tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
+      leafletOutput("mymap")
+    )
   )
+)
